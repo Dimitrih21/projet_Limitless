2b9527df917eeeedf3ed84b1db319f6dac5502d9
Dimitrih21 <d.hoerth@codeur.online>
2020-09-14 11:03:18 +0200

    Projet Limitless

---
+++ myfirstproject/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href=" main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">Dur dur le début de formation !</p>
+  <hr class="my-4">
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+    <div class="row">
+      <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRUbJLfL1vFRcAra8uBVQK_8vu0k3K3PUmIW4H6o2s9-_TiyC3Z&usqp=CAU" class="card-img-top" alt="univers">
+  <div class="card-body">
+    <h5 class="card-title">L'univers1</h5>
+      <p class="card-text">Blablabla</p>
+        <a href="#" class="btn btn-primary">Clic clic</a>
+      </div>
+</div>
+      </div>
+        <div class="col-sm">
+          <div class="card" style="width: 18rem;">
+            <img src="https://images.radio-canada.ca/q_auto,w_1250/v1/ici-info/16x9/illustration-univers-planete-cosmos.jpg" class="card-img-top" alt="univers">
+              <div class="card-body">
+    <h5 class="card-title">L'univers2</h5>
+    <p class="card-text">C'est pas si simple !</p>
+    <a href="#" class="btn btn-primary">Clic clic</a>
+  </div>
+</div>
+  </div>
+      <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRYF7CR5pS00y8eiHBV1j_-6hWSnfU0Ka4jUifqjmUKaV9HR54l&usqp=CAU" class="card-img-top" alt="univers">
+  <div class="card-body">
+    <h5 class="card-title">L'univers3</h5>
+    <p class="card-text">Mais on s'accroche !</p>
+    <a href="#" class="btn btn-primary">Clic clic</a>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
